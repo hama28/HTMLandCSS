ac6af9edbb663a5df0d1e8746c73749454508d23
hata <mh.mundus@gmail.com>
2020-07-07 13:17:55 +0900

    7/7

---
+++ polytech-site/test.js
@@ -0,0 +1,56 @@
+
+document.getElementById('btn1').onclick = function() {
+    document.getElementById('first-wrapper').style.display = "none";
+    document.getElementById('job-wrapper').style.display = "flex";
+}
+document.getElementById('btn2').onclick = function() {
+    document.getElementById('first-wrapper').style.display = "none";
+    document.getElementById('skill-wrapper').style.display = "flex";
+}
+
+document.getElementById('btn3-1').onclick = function() {
+    document.getElementById('it-course').style.display = "block";
+    document.getElementById('restart').style.display = "flex";
+    document.getElementById('job-wrapper').style.display = "none";
+}
+document.getElementById('btn3-2').onclick = function() {
+    document.getElementById('it-course').style.display = "block";
+    document.getElementById('restart').style.display = "flex";
+    document.getElementById('job-wrapper').style.display = "none";
+}
+document.getElementById('btn3-3').onclick = function() {
+    document.getElementById('it-course').style.display = "block";
+    document.getElementById('restart').style.display = "flex";
+    document.getElementById('job-wrapper').style.display = "none";
+}
+document.getElementById('btn4-1').onclick = function() {
+    document.getElementById('it-course').style.display = "block";
+    document.getElementById('restart').style.display = "flex";
+    document.getElementById('skill-wrapper').style.display = "none";
+}
+document.getElementById('btn4-2').onclick = function() {
+    document.getElementById('it-course').style.display = "block";
+    document.getElementById('restart').style.display = "flex";
+    document.getElementById('skill-wrapper').style.display = "none";
+}
+document.getElementById('btn4-3').onclick = function() {
+    document.getElementById('it-course').style.display = "block";
+    document.getElementById('restart').style.display = "flex";
+    document.getElementById('skill-wrapper').style.display = "none";
+}
+document.getElementById('btn4-4').onclick = function() {
+    document.getElementById('it-course').style.display = "block";
+    document.getElementById('restart').style.display = "flex";
+    document.getElementById('skill-wrapper').style.display = "none";
+}
+document.getElementById('btn4-5').onclick = function() {
+    document.getElementById('it-course').style.display = "block";
+    document.getElementById('restart').style.display = "flex";
+    document.getElementById('skill-wrapper').style.display = "none";
+}
+
+document.getElementById('resbtn').onclick = function() {
+    document.getElementById('it-course').style.display = "none";
+    document.getElementById('restart').style.display = "none";
+    document.getElementById('first-wrapper').style.display = "flex";
+}
