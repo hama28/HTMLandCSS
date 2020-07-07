aa64d0f1b2de6b6dc251de779198d787cd6effa4
hata <mh.mundus@gmail.com>
2020-07-07 15:15:06 +0900

    7/7

--- polytech-site/test.js
+++ polytech-site/test.js
@@ -1,3 +1,5 @@
+//もっと簡単に書けると思うんじゃー(# ﾟДﾟ)
+//書き方が分からん、チクショー！
 
 document.getElementById('btn1').onclick = function() {
     document.getElementById('first-wrapper').style.display = "none";
@@ -20,6 +22,21 @@ document.getElementById('btn3-2').onclick = function() {
 }
 document.getElementById('btn3-3').onclick = function() {
     document.getElementById('it-course').style.display = "block";
+    document.getElementById('restart').style.display = "flex";
+    document.getElementById('job-wrapper').style.display = "none";
+}
+document.getElementById('btn3-4').onclick = function() {
+    document.getElementById('iot-course').style.display = "block";
+    document.getElementById('restart').style.display = "flex";
+    document.getElementById('job-wrapper').style.display = "none";
+}
+document.getElementById('btn3-5').onclick = function() {
+    document.getElementById('iot-course').style.display = "block";
+    document.getElementById('restart').style.display = "flex";
+    document.getElementById('job-wrapper').style.display = "none";
+}
+document.getElementById('btn3-6').onclick = function() {
+    document.getElementById('iot-course').style.display = "block";
     document.getElementById('restart').style.display = "flex";
     document.getElementById('job-wrapper').style.display = "none";
 }
@@ -49,8 +66,35 @@ document.getElementById('btn4-5').onclick = function() {
     document.getElementById('skill-wrapper').style.display = "none";
 }
 
+document.getElementById('btn5-1').onclick = function() {
+    document.getElementById('iot-course').style.display = "block";
+    document.getElementById('restart').style.display = "flex";
+    document.getElementById('skill-wrapper').style.display = "none";
+}
+document.getElementById('btn5-2').onclick = function() {
+    document.getElementById('iot-course').style.display = "block";
+    document.getElementById('restart').style.display = "flex";
+    document.getElementById('skill-wrapper').style.display = "none";
+}
+document.getElementById('btn5-3').onclick = function() {
+    document.getElementById('iot-course').style.display = "block";
+    document.getElementById('restart').style.display = "flex";
+    document.getElementById('skill-wrapper').style.display = "none";
+}
+document.getElementById('btn5-4').onclick = function() {
+    document.getElementById('iot-course').style.display = "block";
+    document.getElementById('restart').style.display = "flex";
+    document.getElementById('skill-wrapper').style.display = "none";
+}
+document.getElementById('btn5-5').onclick = function() {
+    document.getElementById('iot-course').style.display = "block";
+    document.getElementById('restart').style.display = "flex";
+    document.getElementById('skill-wrapper').style.display = "none";
+}
+
 document.getElementById('resbtn').onclick = function() {
     document.getElementById('it-course').style.display = "none";
+    document.getElementById('iot-course').style.display = "none";
     document.getElementById('restart').style.display = "none";
     document.getElementById('first-wrapper').style.display = "flex";
 }
